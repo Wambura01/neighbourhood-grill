42af30d253f204557713c1d50c2c8a3a9cfc841d
Wambura <brianwambura96@gmail.com>
2020-11-11 23:29:03 +0300

    Added react-router-dom for navigation within the site

--- src/Components/Directory/directory.component.jsx
+++ src/Components/Directory/directory.component.jsx
@@ -14,28 +14,33 @@ class Directory extends React.Component {
 					title: 'starters',
 					imageUrl: 'https://i.ibb.co/YT9tV40/sausages.jpg',
 					id: 1,
+					linkUrl: 'starters',
 				},
 				{
 					title: 'sides',
 					imageUrl: 'https://i.ibb.co/hCmKQSM/fries.jpg',
 					id: 2,
+					linkUrl: 'sides',
 				},
 				{
 					title: 'platters',
 					imageUrl: 'https://i.ibb.co/pwPx5Cq/platter.jpg',
 					id: 3,
+					linkUrl: 'platters',
 				},
 				{
 					title: 'chicken',
 					imageUrl: 'https://i.ibb.co/rsVGdqX/fried-chicken.jpg',
 					size: 'large',
 					id: 4,
+					linkUrl: 'chicken',
 				},
 				{
 					title: 'pork',
 					imageUrl: 'https://i.ibb.co/xjkcKk0/pork-ribs.jpg',
 					size: 'large',
 					id: 5,
+					linkUrl: 'pork',
 				},
 			],
 		};
@@ -44,8 +49,8 @@ class Directory extends React.Component {
 	render() {
 		return (
 			<div className="directory-menu">
-				{this.state.sections.map(({ title, imageUrl, id, size }) => (
-					<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+				{this.state.sections.map(({ id, ...otherSectionProps }) => (
+					<MenuItem key={id} {...otherSectionProps} />
 				))}
 			</div>
 		);
