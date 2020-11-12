475addf9cf3ead9f0ee097a616842e583272f086
Wambura <brianwambura96@gmail.com>
2020-11-12 18:34:20 +0300

    Added the sign in and sign up page and added the sign component and styled it. Added its handle change functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/Shop/shop.component';
 import Header from './Components/Header/header.component';
+import SignInandSignUpPage from './Pages/Sign-In-and-Sign-Up/sign-in-and-sign-up.component';
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
+				<Route path="/signin" component={SignInandSignUpPage} />
 			</Switch>
 		</div>
 	);
